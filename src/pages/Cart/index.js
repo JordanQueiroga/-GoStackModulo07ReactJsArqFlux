5a10e8439893d8f2b8dd47740079cfe28489d148
Jordan Queiroga <jordanpqueiroga@gmail.com>
2020-03-22 08:30:18 -0300

    Configurando redux, reactotron e estilizando nossa paginas com styled-components

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,71 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://imgcentauro-a.akamaihd.net/900x900/92344102/tenis-new-balance-ml501-masculino-img.jpg"
+                alt="Tênis New Balance ML501 - Masculino"
+              />
+            </td>
+            <td>
+              <strong>Tênis New Balance ML501 - Masculino</strong>
+              <span>R$129,90</span>
+            </td>
+
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$259,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
